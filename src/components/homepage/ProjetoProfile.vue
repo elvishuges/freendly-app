7088bb8549da893c5a2e25cfc9c621b9f9fc0c09
Elvis <elvishuges@hotmail.com>
2020-08-20 00:03:53 -0300

    sedding messate to the server using socket

--- src/components/homepage/ProjetoProfile.vue
+++ src/components/homepage/ProjetoProfile.vue
@@ -3,7 +3,7 @@
     <v-row>
       <v-flex xs12 sm8 md9>Corpo</v-flex>
       <v-flex xs12 sm4 md3>
-        <v-card height="600px" class="overflow-hidden">
+        <v-card height="595px" class="overflow-hidden">
           <v-toolbar color="white" flat>
             <v-toolbar-title class="teal--text text--darken-3">Coversa com participante</v-toolbar-title>
 
@@ -18,64 +18,49 @@
 
           <v-card-text>
             <v-row>
-              <v-card height="350px" class="scroll" flat>
-                <v-col cols="12">
+              <v-card  ref="chat" height="350px" class="scroll" flat>
+                <v-col cols="12" :key="i" v-for="(item, i) in messages">
                   <v-card max-width="100%">
                     <v-card-text class="font-weight-medium">
-                      Hello, is there any body in there ? Just notify if you can hear me.
+                      {{item.text}}
                       <v-row justify="end">
                         <span class="ml-4 px-1 caption grey--text">2:25pm</span>
                       </v-row>
                     </v-card-text>
                   </v-card>
                 </v-col>
-                <v-col cols="12">
-                  <v-card max-width="100%">
-                    <v-card-text class="font-weight-medium">
-                      Hello, is there any body in there ? Just notify if you can hear me.
-                      <v-row justify="end">
-                        <span class="ml-4 px-1 caption grey--text">2:25pm</span>
-                      </v-row>
-                    </v-card-text>
-                  </v-card>
-                </v-col>
-                <v-col cols="12">
-                  <v-card max-width="100%">
-                    <v-card-text class="font-weight-medium">
-                      Hello, is there any body in there ? Just notify if you can hear me.
-                      <v-row justify="end">
-                        <span class="ml-4 px-1 caption grey--text">2:25pm</span>
-                      </v-row>
-                    </v-card-text>
-                  </v-card>
-                </v-col>
-                <v-col xs="12">
+                 <!-- <v-col xs="12">
                   <v-row justify="end" align="end">
                     <v-card :color="$vuetify.theme.dark ? '' : 'green lighten-4'">
                       <v-card-text class="font-weight-medium">
                         <span>tripnu pattiya nalla places athokkeyanu ?</span>
-                        <v-row justify="end">
-                          <span class="ml-4 px-1 caption grey--text">2:25pm</span>
-                        </v-row>
+                        <v-row justify="end"><span class="ml-4 px-1 caption grey--text">2:25pm</span></v-row>
                       </v-card-text>
                     </v-card>
                   </v-row>
-                </v-col>
+                </v-col> -->
               </v-card>
             </v-row>
 
             <v-row>
-              <v-col cols="12">
+              <v-col cols="9">
                 <v-text-field
                   prepend-icon="mdi-emoticon"
-                  append-outer-icon="mdi-send"
+                  :append-icon-cb="sendMessage"
                   filled
+                  v-model="inputChatText"
                   clear-icon="mdi-close-mesasge"
                   clearable
                   label="digite sua mensage"
                   type="text"
                 ></v-text-field>
+
               </v-col>
+              <v-col cols="2" class="pl-3">
+               <v-btn @click="sendMessage(inputChatText)" class="ma-2" text icon>
+                  <v-icon>mdi-send</v-icon>
+                </v-btn>
+                </v-col>
             </v-row>
           </v-card-text>
         </v-card>
@@ -88,21 +73,62 @@
 export default {
   data() {
     return {
-        inset: false,
+      inset: false,
+      inputChatText:"",
       items: [
         { title: "Home", icon: "mdi-account" },
         { title: "My Account", icon: "mdi-account" },
         { title: "Users", icon: "mdi-account" },
       ],
+      messages: [ ],
     };
   },
-  mounted() {},
+
+  sockets: {
+    connect() {
+      // Fired when the socket connects.
+      this.isConnected = true;
+    },
+
+    disconnect() {
+      this.isConnected = false;
+    },
+
+    // Fired when the server sends something on the "messageChannel" channel.
+    chatChannel(data) {
+      let serverMessage = {text:data.msg}
+      this.messages.push(serverMessage)
+      this.socketMessage = data
+    }
+  },
+  mounted() {
+     //this.addMessagesTeste()
+     this.$socket.emit('onProjectPage', 1) // 1 de teste para id do usuário mas seria user.id
+  },
 
   methods: {
-    name() {},
+    sendMessage(message) {
+      let msg = { text:message }
+      let socketMessage = { usr:2,msg:message }
+      this.$socket.emit('chat', socketMessage)
+      this.messages.push(msg)
+    },
+    addMessagesTeste(){
+      setInterval(() => {
+       let m = { text: 'Oi'}
+       this.messages.push(m)
+      }, 2000);
+    }
   },
 
   computed: {},
+  watch: {
+    messages() {
+      setTimeout(() => {
+        this.$refs.chat.$el.scrollTop = this.$refs.chat.$el.scrollHeight;
+      }, 0);
+    }
+  },
   created() {
     console.log("route", this.$route.params.idProjeto);
   },
--- src/main.js
+++ src/main.js
@@ -6,16 +6,16 @@ import router from "./router";
 import store from  './store'
 //import SocketIO from 'socket.io-client'; // "socket.io-client": "^2.0.4"
 //import * as io from 'socket.io-client'
-// import VueSocketIO from 'vue-socket.io';
+import VueSocketIO from 'vue-socket.io';
 
-// Vue.config.productionTip = false
+Vue.config.productionTip = false
 
-// //Vue.use(VueSocketIO,'http://localhost:3000', store)
+//Vue.use(VueSocketIO,'http://localhost:3000', store)
 
-// Vue.use(new VueSocketIO({
-//   debug: true,
-//   connection: 'http://localhost:3000',
-// }));
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: 'http://localhost:3000',
+}));
 
 
 
