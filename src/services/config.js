7b2bc12448a72df6fa9bdaab682d1bdfb0830168
elvishuges <elvishuges@hotmail.com>
2021-02-15 01:00:23 -0300

    atualizacoes de .env variavieis

--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,8 @@
 
 // eslint-disable-next-line
 /* eslint-disable */
-export const freendlyApiBaseURL = 'https://sistemahuges-api.herokuapp.com/'
+export const freendlyApiBaseURL = process.env.VUE_APP_API
+//'process.env.VUE_APP_API'
+//'https://sistemahuges-api.herokuapp.com/'
 //export const freendlyApiBaseURL = 'http://localhost:3000/'
 export const telegrambaseBotURL = 'https://simplesmedic.herokuapp.com/'
