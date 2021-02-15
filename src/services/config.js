6fabb3d83d7da322eebe790f1e2df91305cd41e5
elvishuges <elvishuges@hotmail.com>
2021-02-14 23:56:53 -0300

    ignorando .env

--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,6 @@
 
 // eslint-disable-next-line
 /* eslint-disable */
-export const freendlyApiBaseURL = process.env.VUE_APP_API
+export const freendlyApiBaseURL = 'https://sistemahuges-api.herokuapp.com/'
 //export const freendlyApiBaseURL = 'http://localhost:3000/'
 export const telegrambaseBotURL = 'https://simplesmedic.herokuapp.com/'
